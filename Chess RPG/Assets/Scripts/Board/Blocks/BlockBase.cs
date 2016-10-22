d1e271f05ffc9831b0f1f327353c77d9fa188f5a
BLUDRAG <kearan.petersen@gmail.com>
2016-10-22 14:37:11 +0200

    Added the initial Board and BlockBase classes.

---
+++ Chess RPG/Assets/Scripts/Board/Blocks/BlockBase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Gamify
+{
+	/// <summary>
+	/// The BlockBase defines skeleton methods available to
+	/// all derived Block types. Only methods defined here
+	/// should be used in other classes not derived from
+	/// BlockBase. 
+	/// </summary>
+	public abstract class BlockBase : ScriptableObject {
+		
+		/// <summary>
+		/// Handles events when this block is highlighted.
+		/// </summary>
+		public abstract void OnHighlight(GameObject block);
+		
+		/// <summary>
+		/// Handles events when this block is selected.
+		/// </summary>
+		public abstract void OnSelected(GameObject block);
+	}
+}
---
+++ Chess RPG/Assets/Scripts/Board/Board.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Gamify
+{
+	public class Board : MonoBehaviour {
+	
+		public List<BlockBase> Blocks = new List<BlockBase>();
+	}
+}
