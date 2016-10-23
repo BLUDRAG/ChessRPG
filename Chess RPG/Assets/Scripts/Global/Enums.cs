8e744e9c2df01f44c00c4b01c3be07b9ea0f38d5
BLUDRAG <kearan.petersen@gmail.com>
2016-10-23 18:10:38 +0200

    Added the initial Enums class and moved the EventManager into the Scripts/Global directory.

---
+++ Chess RPG/Assets/Scripts/Global/Enums.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Gamify
+{
+	/// <summary>
+	/// The purpose of this class is to act as a container
+	/// for all globally used enums.
+	/// </summary>
+	public class Enums {
+		
+		/// <summary>
+		/// Contains all the rank values on the board.
+		/// </summary>
+		public enum Ranks
+		{
+			_1 = 1,
+			_2 = 2,
+			_3 = 3,
+			_4 = 4,
+			_5 = 5,
+			_6 = 6,
+			_7 = 7,
+			_8 = 8
+		}
+		
+		/// <summary>
+		/// Contains all the file values on the board.
+		/// </summary>
+		public enum Files
+		{
+			A = 1,
+			B = 2,
+			C = 3,
+			D = 4,
+			E = 5,
+			F = 6,
+			G = 7,
+			H = 8
+		}
+	}
+}
