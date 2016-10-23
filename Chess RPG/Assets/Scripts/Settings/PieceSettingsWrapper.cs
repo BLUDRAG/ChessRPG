b1d89eb49f488c79ffbb9e02dcf3a288ab31d9c6
BLUDRAG <kearan.petersen@gmail.com>
2016-10-23 19:39:57 +0200

    Added the initial PieceSettingsWrapper class.

---
+++ Chess RPG/Assets/Scripts/Settings/PieceSettingsWrapper.cs
@@ -0,0 +1,16 @@
+﻿namespace Gamify
+{
+	/// <summary>
+	/// The sole purpose of this class is to wrap all
+	/// information needed to create an individual
+	/// piece setting inside a single container.
+	/// </summary>
+	public class PieceSettingsWrapper {
+	
+		public Enums.PieceType Type;
+		
+		public Enums.Files File;
+		
+		public Enums.Ranks Rank;
+	}
+}
