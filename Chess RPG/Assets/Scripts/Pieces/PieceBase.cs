f42040feb8638449d538dc2956c6a1b1b7130111
BLUDRAG <kearan.petersen@gmail.com>
2016-10-23 17:55:14 +0200

    Added the initial PieceBase class.

---
+++ Chess RPG/Assets/Scripts/Pieces/PieceBase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Gamify
+{
+	/// <summary>
+	/// The PieceBase defines skeleton methods available to
+	/// all derived Piece types. Only methods defined here
+	/// should be used in other classes not derived from
+	/// PieceBase. 
+	/// </summary>
+	public abstract class PieceBase {
+	
+		/// <summary>
+		/// Handles events when this piece is highlighted.
+		/// </summary>
+		public abstract void OnHighlight(GameObject piece);
+		
+		/// <summary>
+		/// Handles events when this piece is selected.
+		/// </summary>
+		public abstract void OnSelected(GameObject piece);
+	}
+}
