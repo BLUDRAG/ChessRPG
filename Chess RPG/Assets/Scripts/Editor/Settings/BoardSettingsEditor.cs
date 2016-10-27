859e8d24bfc1bea6c44816985ebbace3dd17c9a3
BLUDRAG <kearan.petersen@gmail.com>
2016-10-27 06:51:32 +0200

    Adjusted the visual appearance of board settings to match the real time board a bit better.

--- Chess RPG/Assets/Scripts/Editor/Settings/BoardSettingsEditor.cs
+++ Chess RPG/Assets/Scripts/Editor/Settings/BoardSettingsEditor.cs
@@ -2,6 +2,7 @@
 using UnityEditor;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Gamify
 {
@@ -32,54 +33,109 @@ namespace Gamify
 			headerStyle.fontStyle = FontStyle.Bold;
 			headerStyle.alignment = TextAnchor.UpperCenter;
 			
-			foreach(Enums.Files file in Enum.GetValues(typeof(Enums.Files)))
+			EditorGUILayout.LabelField("Board Layout", headerStyle);
+			
+			Dictionary<Enums.Files, List<BlockSettingsWrapper>> orderedBoardArrangement = new Dictionary<Gamify.Enums.Files, List<Gamify.BlockSettingsWrapper>>();
+			Array fileTypes = Enum.GetValues(typeof(Enums.Files));
+			
+			foreach(Enums.Files file in fileTypes)
+			{ 
+				orderedBoardArrangement[file] = new List<Gamify.BlockSettingsWrapper>();
+				orderedBoardArrangement[file].AddRange(_settings.Blocks.FindAll(block => block.File == file));
+			}
+			
+			for(int i = fileTypes.Length - 1; i >= 0; i--)
 			{
-				EditorGUILayout.LabelField(file.ToString() + " File", headerStyle);
+				Enums.Files currentType = (Enums.Files)fileTypes.GetValue(i);
+				
+				List<BlockSettingsWrapper> filteredFile = GetDefaultFile(currentType);
+				orderedBoardArrangement[currentType].ForEach(wrapper => filteredFile[((int)wrapper.Rank) - 1] = wrapper);
 				
 				EditorGUILayout.BeginHorizontal();
 				
-				GUILayout.Label("Colour", EditorStyles.boldLabel);
-				GUILayout.Label("File", EditorStyles.boldLabel);
-				GUILayout.Label("Rank", EditorStyles.boldLabel);
+				GUILayout.Label(currentType.ToString());
 				
-				EditorGUILayout.EndHorizontal();
-				
-				foreach(BlockSettingsWrapper block in _settings.Blocks.FindAll(block => block.File == file))
+				foreach(BlockSettingsWrapper block in filteredFile)
 				{
 					EditorGUI.BeginChangeCheck();
 					
-					EditorGUILayout.BeginHorizontal();
 					
-					Enums.BlockColour newColour = (Enums.BlockColour)EditorGUILayout.EnumPopup(block.Colour);
-					Enums.Files newFile = (Enums.Files)EditorGUILayout.EnumPopup(block.File);
-					Enums.Ranks newRank = (Enums.Ranks)EditorGUILayout.EnumPopup(block.Rank);
 					
-					if(GUILayout.Button("Remove"))
+					GUIStyle blockStyle = new GUIStyle(GUI.skin.button);
+					
+					switch(block.Colour)
 					{
-						_settings.Blocks.Remove(block);
-						
-						EditorUtility.SetDirty(_settings);
-					}
+					case Enums.BlockColour.Black:
+						GUI.backgroundColor = Color.black;
+						break;
+					case Enums.BlockColour.White:
+						GUI.backgroundColor = Color.white;
+						break;
+					}										
 					
-					EditorGUILayout.EndHorizontal();
+					if(GUILayout.Button("", blockStyle))
+					{
+						string[] options = new string[]{"A", "B"};
+						
+						EditorGUILayout.Popup(0, options);
+					}
 					
 					if(EditorGUI.EndChangeCheck())
 					{
-						block.Colour = newColour;
-						block.File = newFile;
-						block.Rank = newRank;
+						
 					}
-					
-					EditorUtility.SetDirty(_settings);
 				}
 				
-				if(GUILayout.Button("Add Block To " + file.ToString() + " File"))
-				{
-					_settings.Blocks.Add(new BlockSettingsWrapper(Enums.BlockColour.White, file, Enums.Ranks._1));
-					
-					EditorUtility.SetDirty(_settings);
-				}
+				EditorGUILayout.EndHorizontal();
 			}
+			
+			//foreach(BlockSettingsWrapper block in _settings.Blocks.FindAll(block => block.File == file))
+			//{										
+			//	EditorGUI.BeginChangeCheck();
+				
+			//	EditorGUILayout.BeginHorizontal();
+				
+			//	Enums.BlockColour newColour = (Enums.BlockColour)EditorGUILayout.EnumPopup(block.Colour);
+			//	Enums.Files newFile = (Enums.Files)EditorGUILayout.EnumPopup(block.File);
+			//	Enums.Ranks newRank = (Enums.Ranks)EditorGUILayout.EnumPopup(block.Rank);
+				
+			//	if(GUILayout.Button("Remove"))
+			//	{
+			//		_settings.Blocks.Remove(block);
+					
+			//		EditorUtility.SetDirty(_settings);
+			//	}
+				
+			//	EditorGUILayout.EndHorizontal();
+				
+			//	if(EditorGUI.EndChangeCheck())
+			//	{
+			//		block.Colour = newColour;
+			//		block.File = newFile;
+			//		block.Rank = newRank;
+			//	}
+				
+			//	EditorUtility.SetDirty(_settings);
+			//}
+			
+			//if(GUILayout.Button("Add Block To " + file.ToString() + " File"))
+			//{
+			//	_settings.Blocks.Add(new BlockSettingsWrapper(Enums.BlockColour.White, file, Enums.Ranks._1));
+				
+			//	EditorUtility.SetDirty(_settings);
+			//}
+		}
+		
+		public List<BlockSettingsWrapper> GetDefaultFile(Enums.Files file)
+		{
+			List<BlockSettingsWrapper> defaultFile = new List<Gamify.BlockSettingsWrapper>();
+			
+			for(int i = 0; i < Enum.GetValues(typeof(Enums.Files)).Length; i++)
+			{
+				defaultFile.Add(new BlockSettingsWrapper(Enums.BlockColour.White, file, (Enums.Ranks)(i + 1)));
+			}
+			
+			return defaultFile;
 		}
 	}
 }
