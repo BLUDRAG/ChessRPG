5056ff271fdb6ea436a61d36dbbca7084ab2d9cf
BLUDRAG <kearan.petersen@gmail.com>
2016-12-07 20:19:29 +0200

    Added the BaseMaterial class in preparation for adding plug and play highlight effects to pieces and blocks. Implemented the initial MaterialHighlight class.

---
+++ Chess RPG/Assets/Scripts/Effects/Highlights/BaseHighlight.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Gamify
+{
+	/// <summary>
+	/// This class defines the functionality skeleton available
+	/// to all highlight effects. Only method defined here should
+	/// be used in other classes.
+	/// </summary>
+	public abstract class BaseHighlight : MonoBehaviour {
+
+        /// <summary>
+        /// Flag indicating whether this highlight effect is attached to a piece or a block.
+        /// </summary>
+        private bool _isPiece = false;
+
+        protected virtual void Start()
+        {
+            // Determine which highlight event to register to
+            _isPiece = GetComponentInParent<PieceBase>() != null;
+
+            // Register highlight events
+            EventManager<bool>.SubscribeEvent(_isPiece ? EventDefinitions.PieceHighlighted : EventDefinitions.BlockHighlighted, OnHighlight);
+            EventManager<bool>.SubscribeEvent(_isPiece ? EventDefinitions.PieceUnhighlighted : EventDefinitions.BlockUnhighlighted, OnHighlight);
+        }
+
+        protected virtual void OnDestroy()
+        {
+            // Remove events
+            EventManager<bool>.UnsubscribeEvent(_isPiece ? EventDefinitions.PieceHighlighted : EventDefinitions.BlockHighlighted, OnHighlight);
+            EventManager<bool>.UnsubscribeEvent(_isPiece ? EventDefinitions.PieceUnhighlighted : EventDefinitions.BlockUnhighlighted, OnHighlight);
+        }
+		
+		/// <summary>
+		/// The effect that occurs when the object's OnHighlight event is called.
+		/// </summary>
+		public abstract void OnHighlight();
+		
+		/// <summary>
+		/// The effect that occurs when the object's OnUnhighlight event is called.
+		/// </summary>
+		public abstract void OnUnhighlight();
+	}
+}
---
+++ Chess RPG/Assets/Scripts/Effects/Highlights/MaterialHighlight.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Gamify
+{
+	public class MaterialHighlight : BaseHighlight {
+		
+		/// <summary>
+		/// The effect hat occurs when the object's OnHighlight event is called.
+		/// </summary>
+		public override void OnHighlight()
+		{
+			throw new System.NotImplementedException();
+		}
+		
+		/// <summary>
+		/// The effect hat occurs when the object's OnUnhighlight event is called.
+		/// </summary>
+		public override void OnUnhighlight()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
